d4ab75852a5ef7f0021594ff445431784a8c3187
LuanPV <phamluanhanu2010@gmail.com>
2021-09-06 23:47:15 +0700

    [Update] Plugin

--- android/src/main/kotlin/com/example/plugin_codelab/PluginCodelabPlugin.kt
+++ android/src/main/kotlin/com/example/plugin_codelab/PluginCodelabPlugin.kt
@@ -15,65 +15,33 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 
 /** PluginCodelabPlugin */
-class PluginCodelabPlugin: FlutterActivity() {
-  private val CHANNEL = "plugin_codelab"
-  private val RECORD_VIDEO_ACTIVITY_REQUEST_CODE = 10001
-  private lateinit var resultMethodChanel: MethodChannel.Result
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    GeneratedPluginRegister.registerGeneratedPlugins(FlutterEngine(baseContext))
-    MethodChannel(
-      flutterEngine?.dartExecutor?.binaryMessenger,
-      CHANNEL
-    ).setMethodCallHandler { call, result ->
-      if (call.method.equals("getPlatformVersion")) {
-        resultMethodChanel = result
-        startRecordActivity()
-      } else {
-        result.notImplemented()
-      }
-    }
-
-  }
-
-  private fun startRecordActivity() {
-//        val intent = Intent(this, RecordActivity::class.java)
-    val intent = Intent(this, RecordActivity::class.java)
-    startActivityForResult(intent, RECORD_VIDEO_ACTIVITY_REQUEST_CODE)
-  }
-
-  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-    super.onActivityResult(requestCode, resultCode, data)
-    if (requestCode == RECORD_VIDEO_ACTIVITY_REQUEST_CODE) {
-      resultMethodChanel.success("Close record video activity")
-    }
-  }
-}
-
-//  FlutterPlugin, MethodCallHandler {
+class PluginCodelabPlugin:
+// FlutterActivity() {
+//  private val CHANNEL = "plugin_codelab"
 //  private val RECORD_VIDEO_ACTIVITY_REQUEST_CODE = 10001
 //  private lateinit var resultMethodChanel: MethodChannel.Result
-//  /// The MethodChannel that will the communication between Flutter and native Android
-//  ///
-//  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
-//  /// when the Flutter Engine is detached from the Activity
-//  private lateinit var channel : MethodChannel
 //
-//  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
-//    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "plugin_codelab")
-//    channel.setMethodCallHandler(this)
+//  override fun onCreate(savedInstanceState: Bundle?) {
+//    super.onCreate(savedInstanceState)
+//    GeneratedPluginRegister.registerGeneratedPlugins(FlutterEngine(baseContext))
+//    MethodChannel(
+//      flutterEngine?.dartExecutor?.binaryMessenger,
+//      CHANNEL
+//    ).setMethodCallHandler { call, result ->
+//      if (call.method.equals("getPlatformVersion")) {
+//        resultMethodChanel = result
+//        startRecordActivity()
+//      } else {
+//        result.notImplemented()
+//      }
+//    }
+//
 //  }
 //
-//  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-//    if (call.method == "getPlatformVersion") {
-//      resultMethodChanel = result
-//      val intent = Intent(this, RecordActivity::class.java)
-//      startActivityForResult(intent, RECORD_VIDEO_ACTIVITY_REQUEST_CODE)
-////      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-//    } else {
-//      result.notImplemented()
-//    }
+//  private fun startRecordActivity() {
+////        val intent = Intent(this, RecordActivity::class.java)
+//    val intent = Intent(this, RecordActivity::class.java)
+//    startActivityForResult(intent, RECORD_VIDEO_ACTIVITY_REQUEST_CODE)
 //  }
 //
 //  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
@@ -82,8 +50,41 @@ class PluginCodelabPlugin: FlutterActivity() {
 //      resultMethodChanel.success("Close record video activity")
 //    }
 //  }
-//
-//  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
-//    channel.setMethodCallHandler(null)
-//  }
 //}
+
+  FlutterPlugin, MethodCallHandler, FlutterActivity() {
+  private val RECORD_VIDEO_ACTIVITY_REQUEST_CODE = 10001
+  private lateinit var resultMethodChanel: MethodChannel.Result
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private lateinit var channel : MethodChannel
+
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "plugin_codelab")
+    channel.setMethodCallHandler(this)
+  }
+
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    if (call.method == "getPlatformVersion") {
+      resultMethodChanel = result
+      val intent = Intent(this, RecordActivity::class.java)
+      startActivityForResult(intent, RECORD_VIDEO_ACTIVITY_REQUEST_CODE)
+//      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else {
+      result.notImplemented()
+    }
+  }
+
+  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    super.onActivityResult(requestCode, resultCode, data)
+    if (requestCode == RECORD_VIDEO_ACTIVITY_REQUEST_CODE) {
+      resultMethodChanel.success("Close record video activity")
+    }
+  }
+
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
+}
